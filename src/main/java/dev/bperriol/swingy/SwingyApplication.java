9e5df9fa411368721bbd1f734c3b549d4807cea4
draker4 <baptiste.perriolat@live.fr>
2023-10-10 13:14:25 +0200

    initial commit

---
+++ src/main/java/dev/bperriol/swingy/SwingyApplication.java
@@ -0,0 +1,13 @@
+package dev.bperriol.swingy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwingyApplication.class, args);
+	}
+
+}
